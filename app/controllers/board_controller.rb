53aaa30d9e0a7cf3d0a0b5751bc7f44c1e81e850
HMHM1218 <1611510198b@kindai.ac.jp>
2018-06-21 04:12:21 +0900

    board complete

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,10 +1,27 @@
 class BoardController < ApplicationController
-  def new
+  def index
+    @boards = Board.all
   end
 
-  def index
+  def show
+    @board = Board.find(params[:id])
   end
+
+  def new
+    @board = Board.new
+  end
+
 
   def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to @board
+    else
+      render :new
+    end
+  end
+
+  def board_params
+    params.require(:board).permit(:name)
   end
 end
